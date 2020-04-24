c029da49dcb11ee7ab65d4d999687ee18a34c510
wjuanps <wjuan.ps@gmail.com>
2020-04-24 01:53:54 -0300

    update profile view

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
 import logoImg from "../../assets/logo.png";
 import { Link, useHistory } from "react-router-dom";
-import { FiPower, FiTrash2, FiCheck } from "react-icons/fi";
+import {
+  FiPower,
+  FiTrash2,
+  FiEdit2,
+  FiRefreshCcw,
+  FiEye
+} from "react-icons/fi";
 
-import api from "../../services/api";
 import userModel from "../../services/user";
 import eventModel from "../../services/event";
 import "./styles.css";
@@ -12,54 +17,81 @@ export default function Profile() {
   const [events, setEvents] = useState([]);
   const [user, setUser] = useState({});
 
-  const ongId = localStorage.getItem("ongId");
+  const [isDeleting, setDeleting] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [updating, setUpdating] = useState(false);
+  const [eventUpdating, setEventUpdating] = useState("");
+  const [eventDeleting, setEventDeleting] = useState("");
 
   const history = useHistory();
 
   async function fetchUser() {
     try {
+      setLoading(true);
       let response = await userModel.user();
       let { data } = response.data;
 
       setUser(data);
-    } catch (error) {
-      alert("Erro ao carregar dados do usuário");
-    }
-  }
 
-  async function fetchEvents() {
-    try {
-      let response = await eventModel.events();
-      let { data } = response.data;
+      response = await eventModel.events();
+      data = response.data.data;
 
       setEvents(data);
+
+      setUpdating(false);
     } catch (error) {
-      alert("Erro ao carregar eventos");
+      alert("Erro ao carregar dados do usuário");
+    } finally {
+      setLoading(false);
     }
   }
 
   useEffect(() => {
     fetchUser();
-    fetchEvents();
   }, []);
 
-  async function handleDeleteevent(id) {
+  async function handleDeleteEvent(uuid) {
     try {
-      await api.delete(`events/${id}`, {
-        headers: {
-          Authorization: ongId
-        }
-      });
+      setEventDeleting(uuid);
+      setDeleting(true);
+      await eventModel.destroy(uuid);
 
-      setEvents(events.filter(event => event.id !== id));
+      setEvents(events.filter(event => event.uuid !== uuid));
     } catch (err) {
-      alert("Erro ao deletar o caso");
+      alert("Erro ao deletar o evento");
+    } finally {
+      setDeleting(false);
     }
   }
 
   function handleLogout() {
     localStorage.clear();
     window.open("/profile", "_self");
+  }
+
+  function eventConfirmed(uuid, user) {
+    if (user.events.length <= 0) return false;
+
+    let confirmed = user.events.some(
+      event => event.is_confirmed && event.uuid === uuid
+    );
+
+    return confirmed;
+  }
+
+  async function handleConfirmEvent(confirm, uuid) {
+    try {
+      setEventUpdating(uuid);
+      setUpdating(true);
+      const params = new URLSearchParams();
+      params.append("is_confirmed", confirm);
+
+      await eventModel.confirmPresence(uuid, params);
+    } catch (error) {
+      alert("Erro ao atualizar evento");
+    } finally {
+      fetchUser();
+    }
   }
 
   return (
@@ -80,7 +112,16 @@ export default function Profile() {
       <div className="header">
         <h1>Meus Eventos</h1>
 
-        <button className="button-header">Todos os Eventos</button>
+        <button
+          onClick={() => history.push("/events")}
+          className="button-header"
+        >
+          {loading ? (
+            <FiRefreshCcw className="icon-spin" size={22} color="#ffffff" />
+          ) : (
+            "Todos os Eventos"
+          )}
+        </button>
       </div>
 
       <ul>
@@ -97,20 +138,72 @@ export default function Profile() {
               <DateFormat event={event} />
             </p>
 
-            <button
-              className="marcar-presenca"
-              onClick={() => handleDeleteevent(event.id)}
-            >
-              Marcar Presença
-            </button>
+            {eventConfirmed(event.uuid, user) ? (
+              <button
+                onClick={() => handleConfirmEvent(0, event.uuid)}
+                className="presenca-confirmada"
+              >
+                {updating && eventUpdating === event.uuid ? (
+                  <FiRefreshCcw
+                    className="icon-spin"
+                    size={22}
+                    color="#ffffff"
+                  />
+                ) : (
+                  "Desmarcar Presença"
+                )}
+              </button>
+            ) : (
+              <button
+                onClick={() => handleConfirmEvent(1, event.uuid)}
+                className="marcar-presenca"
+              >
+                {updating && eventUpdating === event.uuid ? (
+                  <FiRefreshCcw
+                    className="icon-spin"
+                    size={22}
+                    color="#ffffff"
+                  />
+                ) : (
+                  "Marcar Presença"
+                )}
+              </button>
+            )}
 
-            <button
-              className="delete"
-              type="button"
-              onClick={() => handleDeleteevent(event.id)}
-            >
-              <FiTrash2 size={20} color="#0faab9" />
-            </button>
+            <div>
+              <button
+                className="detail"
+                type="button"
+                onClick={() => history.push(`/events/${event.uuid}/detail`)}
+              >
+                <FiEye size={20} color="#0faab9" />
+              </button>
+
+              <button
+                className="edit"
+                type="button"
+                onClick={() => history.push(`/events/${event.uuid}`)}
+              >
+                <FiEdit2 size={20} color="#0faab9" />
+              </button>
+
+              <button
+                className="delete"
+                type="button"
+                disabled={isDeleting}
+                onClick={() => handleDeleteEvent(event.uuid)}
+              >
+                {isDeleting && eventDeleting === event.uuid ? (
+                  <FiRefreshCcw
+                    className="icon-spin"
+                    size={18}
+                    color="#0faab9"
+                  />
+                ) : (
+                  <FiTrash2 size={20} color="#0faab9" />
+                )}
+              </button>
+            </div>
           </li>
         ))}
       </ul>
@@ -123,22 +216,21 @@ export const DateFormat = ({ event }) => {
   const dateFormated = new Intl.DateTimeFormat("pt-br", {
     year: "numeric",
     month: "2-digit",
-    day: "2-digit"
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit"
   });
   const [
     { value: da },
     ,
     { value: mo },
     ,
-    { value: ye }
+    { value: ye },
+    ,
+    { value: ho },
+    ,
+    { value: mi }
   ] = dateFormated.formatToParts(date);
-
-  const time = new Date(event.event_time);
-  const timeFormated = new Intl.DateTimeFormat("en", {
-    hour: "2-digit",
-    minute: "2-digit"
-  });
-  const [{ value: ho }, , { value: mi }] = timeFormated.formatToParts(time);
 
   return `
     ${da}/${mo}/${ye} ${ho}:${mi}
