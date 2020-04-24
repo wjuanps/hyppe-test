34dceb15b76e48e0624a8e8e6ac6e22c1c432d5c
wjuanps <wjuan.ps@gmail.com>
2020-04-24 01:52:28 -0300

    create event model

--- frontend/src/services/event.js
+++ frontend/src/services/event.js
@@ -1,6 +1,13 @@
 import api from "./api";
 
 export default {
+  list: async () => await api.get("/events"),
   events: async () => await api.get("/user/events"),
-  create: async params => await api.post("/events", params)
+  event: async uuid => await api.get(`/user/events/${uuid}`),
+  datail: async uuid => await api.get(`/events/${uuid}`),
+  update: async (uuid, params) => await api.put(`/events/${uuid}`, params),
+  create: async params => await api.post("/events", params),
+  destroy: async uuid => await api.delete(`/events/${uuid}`),
+  confirmPresence: async (uuid, params) =>
+    await api.put(`/user/events/${uuid}`, params)
 };
