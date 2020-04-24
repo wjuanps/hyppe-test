a9ea1fe60b6eea9df563c1681c13af81d93985de
wjuanps <wjuan.ps@gmail.com>
2020-04-24 01:52:13 -0300

    update app routes

--- frontend/src/routes/app-routes.js
+++ frontend/src/routes/app-routes.js
@@ -2,7 +2,10 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Profile from "../pages/Profile";
+import Events from "../pages/Events";
 import NewEvent from "../pages/NewEvent";
+import DetailEvent from "../pages/DetailEvent";
+import EditEvent from "../pages/EditEvent";
 import NotFound from "../pages/NotFound";
 
 export default function AppRoutes() {
@@ -10,7 +13,10 @@ export default function AppRoutes() {
     <BrowserRouter>
       <Switch>
         <Route path="/profile" exact component={Profile} />
+        <Route path="/events" exact component={Events} />
         <Route path="/events/new" exact component={NewEvent} />
+        <Route path="/events/:uuid" exact component={EditEvent} />
+        <Route path="/events/:uuid/detail" exact component={DetailEvent} />
         <Route path="*" component={NotFound} />
       </Switch>
     </BrowserRouter>
